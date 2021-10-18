7426c3d8900e74f9dd12d197b4747df5df4b7a58
Florian Hug <florian.hug@bs.ch>
2021-10-18 09:02:16 +0200

    merge with existing stuff

---
+++ modification/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>GD Library</title>
+  </head>
+  <body>
+     <figure>
+        <img src="images/original.jpg"/>
+        <figcaption>Original Image</figcaption>
+     </figure>
+  </body>
+</html>
